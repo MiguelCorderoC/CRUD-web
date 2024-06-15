e0ab7df1f25d3f970c374261f01ddbdc447d08c3
Miguel cordero <mikeycordero10@icloud.com>
2024-06-14 19:26:53 -0700

    Funcion de eliminar lista, faltan mejoras

--- cliente/src/views/HomeView.vue
+++ cliente/src/views/HomeView.vue
@@ -4,10 +4,14 @@ import Programa from '@/components/Programa.vue';
 
 <template>
   <main>
+    <section class="sect-opciones">
+      <input type="search">
+      <button>Agregar</button>
+    </section>
     <div v-if="programas.length > 0">
       <div v-for="(programa, index) in programas" :key="index">
         <Programa :titulo="programa.nombre" :descripcion="programa.descripcion" :categoria="programa.categoria"
-          :ano="programa.ano" :portada="programa.portada" />
+          :ano="programa.ano" :portada="programa.portada" :id="programa.id" />
       </div>
     </div>
   </main>
@@ -31,12 +35,30 @@ export default {
       axios.get('http://localhost:3000/api/programas').then(res => {
         this.programas = res.data;
         console.log(res.data);
-      }).catch(err => {
-        console.error(err);
+      }).catch(error => {
+        console.log(error);
       });
     }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.sect-opciones {
+  display: flex;
+  justify-content: right;
+}
+
+.sect-opciones button {
+  background-color: #A5DD9B;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  cursor: pointer;
+  font-size: 1.2em;
+  height: 40px;
+  margin-left: 10px;
+  margin-right: 10px;
+  width: 100px;
+}
+</style>
